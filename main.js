1aeaabe7bd97ac09fcc63d63c85635fdc380ffaf
Isramss <isra.msadak@gmail.com>
2023-12-10 18:53:34 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,113 @@
+// const Mantra = [
+//   {
+//     text: "Je m’aime, je crois en moi-même et je me soutiens quoi qu’il arrive.",
+//   },
+//   {
+//     text: "Je suis aimée, aimante et aimable.",
+//   },
+//   {
+//     text: "Je fais de mon mieux à chaque instant.",
+//   },
+//   {
+//     text: "J’ai en moi les pouvoirs de réalisation de ce que je souhaite.",
+//   },
+//   {
+//     text: "Je suis la personne qui décide de tout ce que je fais et j’ai toujours le choix.",
+//   },
+//   {
+//     text: "Je donne le meilleur de moi-même et je suis comme je suis.",
+//   },
+//   {
+//     text: "Là, maintenant, à cet instant, je vais bien.",
+//   },
+//   {
+//     text: "Je mérite d’être heureuse.",
+//   },
+//   {
+//     text: "L’échec est une étape sur la route du succès.",
+//   },
+//   {
+//     text: "J’accepte ce que je ne peux pas changer afin de me concentrer sur ce que je peux changer.",
+//   },
+//   {
+//     text: "Tout est expérience. J’apprends chaque jour.",
+//   },
+//   {
+//     text: "J’ai confiance en moi et en mes décisions.",
+//   },
+//   {
+//     text: "Je n’ai pas besoin d’être parfaite, j’ai besoin d’être moi-même.",
+//   },
+//   {
+//     text: "Je ne cherche pas à faire plaisir à tout le monde, je fais ce que je pense être bien et juste.",
+//   },
+//   {
+//     text: "Je suis forte, je suis capable, je suis sereine.",
+//   },
+//   {
+//     text: "Ma situation actuelle n’est pas ma destination finale.",
+//   },
+//   {
+//     text: "Je suis force, je suis détermination.",
+//   },
+//   {
+//     text: "L’important est ce que je pense de moi-même, pas ce que les autres pensent de moi.",
+//   },
+//   {
+//     text: "Rien ne peut me résister, je peux tout réussir.",
+//   },
+//   {
+//     text: "Je suis unique, mon parcours est unique.",
+//   },
+//   {
+//     text: "J’apprécie mon esprit, mes émotions, mon corps et mon âme.",
+//   },
+//   {
+//     text: "Toutes mes émotions sont utiles, je les accueille, je les écoute, je m’en libère.",
+//   },
+//   {
+//     text: "Je choisis d’être heureuse. Je choisis de m’aimer.",
+//   },
+//   {
+//     text: "Je vise le progrès, pas la perfection.",
+//   },
+//   {
+//     text: "Plus j’ai confiance en moi, plus j’ai confiance en d’autres personnes.",
+//   },
+//   {
+//     text: "Je suis ravie de mes talents, de mes dons et de mes qualités.",
+//   },
+//   {
+//     text: "Je suis moi, et c’est bien.",
+//   },
+//   {
+//     text: "Je lâche prise et j’accepte ce que je ne peux pas contrôler.",
+//   },
+//   {
+//     text: "Qu’importe les épreuves, j’ai confiance en moi et je le mérite.",
+//   },
+//   {
+//     text: "J’ai déjà réussi, je réussirai encore.",
+//   },
+//   {
+//     text: "Je fais de mon mieux et j’en suis fière.",
+//   },
+//   {
+//     text: "Je travaille sur moi-même, pour moi-même, par moi-même.",
+//   },
+// ];
+
+let Choice1 = document.querySelector("#choice1");
+let Choice2 = document.querySelector("#choice2");
+let btn = document.querySelector(".btn_Message");
+
+btn.addEventListener("click", function (e) {
+  // e.preventDefault();
+  if (Choise1) {
+    alert("super");
+  }
+  if (Choise2) {
+    alert("zero");
+  }
+  alert("super");
+});
